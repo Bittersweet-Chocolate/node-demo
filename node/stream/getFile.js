ef75fbbebc5b425bae35624d00bf828d3886541a
czh-mac <czh533143@163.com>
2022-08-10 17:22:15 +0800

    feat: pipe 使用

---
+++ node/stream/getFile.js
@@ -0,0 +1,6 @@
+const path = require('path')
+
+module.exports = {
+  nameRead: path.resolve(__dirname, '../file/name.txt'),
+  nameWrite: path.resolve(__dirname, '../file/name_write.txt')
+}
---
+++ node/stream/pipe.js
@@ -0,0 +1,8 @@
+const fs = require('fs')
+const { nameRead, nameWrite } = require('./getFile')
+const ReadStream = require('./my-createStream')
+const WriteStream = require('./my-writeStream')
+// 官方读取是64k读取，16k写入。读取写入比例4:1
+
+// 可读流.pipe(可写流) 1.一步的 2.可以实现 读一点写一点
+fs.createReadStream(nameRead).pipe(fs.createWriteStream(nameWrite))
